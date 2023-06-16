8bda673086e1d662d4b2782b1bfe0e7adea7f52c
kpomak <romka_ne@mail.ru>
2023-06-17 02:36:13 +0300

    order and user models

--- customer_back/core/models.py
+++ customer_back/core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+from uuid import uuid4
 
 # Create your models here.
+phone_regex = RegexValidator(
+    regex=r"^\+?1?\d{9,15}$",
+    message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.",
+)
+
+
+class CustomUser(User):
+    phone_number = models.CharField(validators=[phone_regex], max_length=15)
+
+
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    updated = models.DateTimeField(auto_now=True, editable=False)
+    deleted = models.BooleanField(default=False)
