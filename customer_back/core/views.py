1257505d2f8abb54351bcea7675cb7e6c77b12e3
kpomak <romka_ne@mail.ru>
2023-06-17 03:26:28 +0300

    views

--- customer_back/core/views.py
+++ customer_back/core/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.renderers import Jso
+
+from core.models import CustomUser, Order
+from core.serializers import CustomUserModelSerializer, OrderModelSerializer
+
 
 # Create your views here.
+class OrderViewSet(ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderModelSerializer
+
+
+class OrderSnakeCaseViewSet(OrderViewSet):
+    parser_classes = ("rest_framework.parsers.JSONParser",)
+    renderer_classes = ("rest_framework.renderers.JSONRenderer",)
+
+
+class CustomUserViewSet(ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserModelSerializer
+
+
+class CustomUserSnakeCaseViewSet(CustomUserViewSet):
+    parser_classes = ("rest_framework.parsers.JSONParser",)
+    renderer_classes = ("rest_framework.renderers.JSONRenderer",)
