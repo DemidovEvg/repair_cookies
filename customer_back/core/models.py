be3de3e3c9021d66c3f745ff35ee2c7f67284ae3
kpomak <romka_ne@mail.ru>
2023-06-18 13:27:00 +0300

    phon field blank default

--- customer_back/core/models.py
+++ customer_back/core/models.py
@@ -5,14 +5,16 @@ from uuid import uuid4
 
 # Create your models here.
 phone_regex = RegexValidator(
-    regex=r"^\+?1?\d{9,15}$",
+    regex=r"^\d{4,15}$",
     message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.",
 )
 
 
 class Client(AbstractUser):
     phone_number = models.CharField(
-        validators=[phone_regex], max_length=15, unique=True, blank=False
+        validators=[phone_regex],
+        max_length=15,
+        unique=True,
     )
 
 
