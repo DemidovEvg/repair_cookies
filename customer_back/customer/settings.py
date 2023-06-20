c1cdb699e0c4fdbc4b56ae7ed1e6ccacd5c68c3d
kpomak <romka_ne@mail.ru>
2023-06-20 17:28:15 +0300

    whitenoise settings

--- customer_back/customer/settings.py
+++ customer_back/customer/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "customer.urls"
@@ -127,6 +128,12 @@ PHONE_NUMBER_REGION = "RU"
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = (
+    "django.contrib.staticfiles.storage.CompressedManifestStaticFilesStorage"
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
