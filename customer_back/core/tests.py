e02cb15e2746693132c562617ae868d2e592dde3
paul <kpasha11@mai.ru>
2023-07-01 23:21:00 +0300

    создание заказа пользователем и болванка теста

--- customer_back/core/tests.py
+++ customer_back/core/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+"""тестирование создания пользователя"""
+
+
+class AbstractUserTest(TestCase):
+    def test_create_user(self):
+        pass
