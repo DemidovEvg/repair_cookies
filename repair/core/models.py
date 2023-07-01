a1fe30b113362fc23ba1f09ad745a82270023db4
paul <kpasha11@mail.ru>
2023-07-01 21:59:58 +0300

    commit add order and category

--- repair/core/models.py
+++ repair/core/models.py
@@ -1,36 +1,66 @@
-from customer.settings import PHONE_NUMBER_REGION
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.conf import settings
-
-from uuid import uuid4
-
-from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import User
 
 
-# Create your models here.
-class Client(AbstractUser):
-    id = models.UUIDField(verbose_name="Идентификатор", default=uuid4, primary_key=True)
-    phone_number = PhoneNumberField(
-        unique=True, region=PHONE_NUMBER_REGION, max_length=12
-    )
+class ServiceUser(User):
+    def __str__(self):
+        if self.first_name and self.last_name:
+            return f"{self.first_name} {self.last_name}"
+        return self.username
 
 
-class TokenData(models.Model):
+class ServiceMan(models.Model):
     user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
-        verbose_name="Пользователь",
+        ServiceUser,
+        verbose_name="Ремонтник",
         on_delete=models.CASCADE,
         unique=True,
-        related_name="token_data",
+        related_name='serviceman'
     )
-    token = models.CharField(verbose_name="Токен", max_length=1500)
+
+    def __str__(self):
+        return f'Ремонтник {self.user}'
+
+    class Meta:
+        verbose_name = "Ремонтник"
+        verbose_name_plural = "Ремонтники"
+        ordering = ["-id"]
 
 
 class Order(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    status = models.CharField(max_length=255)
-    created = models.DateTimeField(auto_now_add=True, editable=False)
-    updated = models.DateTimeField(auto_now=True, editable=False)
-    deleted = models.BooleanField(default=False)
+    serviceman = models.ForeignKey(
+        ServiceMan,
+        verbose_name="Ремонтник",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    order_num = models.IntegerField(
+        verbose_name='номер заказа',
+        unique=True,
+    )
+    order_task = models.CharField(
+        verbose_name="Неисправность со слов клиента",
+        max_length=1000,
+        default=""
+    )
+    comment = models.CharField(
+        verbose_name="комментарий ремонта",
+        max_length=300,
+        null=True,
+        blank=True,
+    )
+    created = models.DateTimeField(
+        verbose_name="Дата и время начала ремонта", auto_now_add=True, editable=False
+    )
+    updated = models.DateTimeField(
+        verbose_name="Дата и время изменения статуса ремонта", auto_now=True, editable=False
+    )
+
+    def __str__(self):
+        return f"Ремонт id={self.id}"
+
+    class Meta:
+        verbose_name = "Ремонт"
+        verbose_name_plural = "Ремонты"
+        ordering = ["-created"]
