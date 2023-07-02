997530ce05587d5fb912ed655123fb2823a7459e
4uruH <97702288+4uruH@users.noreply.github.com>
2023-07-02 19:16:48 +0300

    Order update view change

--- repair/core/views.py
+++ repair/core/views.py
@@ -1,11 +1,11 @@
-import requests
-import json
 from django.conf import settings
 from django.contrib.auth import logout
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
 from rest_framework.viewsets import ModelViewSet
 from django.views.generic import UpdateView, ListView
 
@@ -13,6 +13,7 @@ from core.models import ServiceMan, Order
 from core.serializers import ServicemanModelSerializer, OrderModelSerializer
 from permissions import ServicemanPermissions, OrderPermissions
 from core.services.order_service import update_outer_order
+from core.serializers import OrderModelSerializer
 
 
 class OrderViewSet(ModelViewSet):
@@ -46,32 +47,22 @@ class OrderDetail(UpdateView):
 
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
-        exceptions = []
-        data = dict(request.POST)
+        payload = OrderModelSerializer(instance=self.object).data
         pk = self.object.id
-        payload = {
-            'serviceman': data.get('serviceman')[0],
-            'serviceman_description': data.get('serviceman_description')[0],
-            'status': data.get('status')[0],
-            'amount_due_by': data.get('amount_due_by')[0],
-        }
 
         try:
             service_update = f"{settings.CLIENT_SERVICE}/api/orders/{pk}/"
             update_outer_order(service_update, payload)
         except Exception as exc:
-            exceptions.append(exc)
+            messages.add_message(request, messages.ERROR, repr(exc))
 
         try:
             service_update = f"{settings.DELIVERY_SERVICE}/api/orders/{pk}/"
             update_outer_order(service_update, payload)
         except Exception as exc:
-            exceptions.append(exc)
+            messages.add_message(request, messages.ERROR, repr(exc))
 
-        return super().post(request, *args, **kwargs)
-
-    def get_success_url(self):
-        return reverse_lazy('home')
+        return HttpResponseRedirect(reverse_lazy('home'))
 
 
 class LoginUser(LoginView):
