930ff3eb2060a33191c3ef0ba46407722f36da1d
paul <kpasha11@mai.ru>
2023-07-09 13:44:25 +0300

    change fail admin, client sync

--- customer_back/core/admin.py
+++ customer_back/core/admin.py
@@ -4,6 +4,7 @@ from django.conf import settings
 
 from core.models import Client, Order
 from core.serializers import OrderModelSerializer
+from core.serializers import ClientModelSerializer
 
 from core.services.order_service import create_or_update
 
@@ -12,7 +13,44 @@ admin.site.site_header = "Клиентская служба"
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("first_name", "phone_number")
+
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        try:
+            self.create_or_update_outer_order(obj)
+        except Exception as exc:
+            messages.add_message(request, messages.ERROR, repr(exc))
+
+    def create_or_update_outer_order(self, order: Order):
+        exceptions = []
+        try:
+            service_create = f"{settings.CLIENT_SERVICE}/api/users/"
+            service_update = f"{service_create}{order.id}/"
+            data = ClientModelSerializer(instance=order).data
+
+            create_or_update(service_create, service_update, data)
+        except Exception as exc:
+            exceptions.append(exc)
+
+        try:
+            service_create = f"{settings.REPAIR_SERVICE}/api/users/"
+            service_update = f"{service_create}{order.id}/"
+            create_or_update(service_create, service_update, data)
+        except Exception as exc:
+            exceptions.append(exc)
+
+        if exceptions:
+            raise Exception(repr(exceptions))
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_add_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    class Meta:
+        ordering = ("username", "phone_number")
 
 
 @admin.register(Order)
